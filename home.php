4117769a2cec7ff1d8217d2528b75ec6827a91d2
Lisesoee <Lise1005>
2017-05-17 15:00:55 +0200

    Continued on main page. Added different parts needed. Made the tables work as flex items to put them next to each other. Played with some CSS

--- home.php
+++ home.php
@@ -10,6 +10,21 @@
 <html>
 <head>
     <style>
+        header {
+            text-align: center;
+            padding: 2%
+        }
+
+        footer {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            padding: 1rem;
+            background-color: #efefef;
+            text-align: center;
+        }
+
         .flex-container {
             display: -webkit-flex;
             display: flex;
@@ -30,100 +45,120 @@
             align-self: flex-start;
             align-content: flex-start;
         }
-
     </style>
-
 </head>
+
+<header>
+    <h1>Unter Dispatcher</h1>
+    <img src="images/taxi.png" alt="taxi picture" style="width:150px;height:99px;"/>
+</header>
+
+
 <body>
 
-<div class="flex-container">
+<!--Tables: -->
+<div class="flex-container" id = "tableSection">
     <div class="flex-item">
-        Customer Name
-        <br>
-        <div class="flex-item">
-            john
-        </div>
+        <!--Table comments:
+        We use thread-tag to make column header look special.
+        When using this, all tr-tags needs to be in a tbody-tag.-->
+        <table id = "requestTable">
+
+            <thead>
+            <tr>
+                <th>Customer Name</th>
+                <th>Phone no</th>
+                <th>Brand preference</th>
+                <th>Priority</th>
+                <th>From address</th>
+                <th>To address</th>
+            </tr>
+            </thead>
+
+
+            <tbody>
+            <tr>
+                <td>Jill Smith</td>
+                <td>12345678</td>
+                <td>Toyota</td>
+                <td>10</td>
+                <td>From this place</td>
+                <td>To this place</td>
+            </tr>
+            <tr>
+                <td>Jill Smith</td>
+                <td>12345678</td>
+                <td>Toyota</td>
+                <td>10</td>
+                <td>From this place</td>
+                <td>To this place</td>
+            </tr>
+            </tbody>
+
+        </table>
     </div>
     <div class="flex-item">
-        Phone No.
-        <br>
-        <div class="flex-item">
-            john number
-        </div>
-    </div>
-    <div class="flex-item">
-        Brand preference
-        <br>
-        <div class="flex-item">
-            john number
-        </div>
-    </div>
-    <div class="flex-item">
-        Priority
-        <br>
-        <div class="flex-item">
-            john number
-        </div>
-    </div>
-    <div class="flex-item">
-        From address
-        <br>
-        <div class="flex-item">
-            john number
-        </div>
-    </div>
-    <div class="flex-item">
-        To address
-        <br>
-        <div class="flex-item">
-            john number
-        </div>
+        <table id = "availableTaxisTable">
+            <thead>
+            <tr>
+                <th>Driver</th>
+                <th>Brand</th>
+                <th>Licence plate</th>
+                <th>Price per km</th>
+
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>Hans Hansen</td>
+                <td>Suzuki</td>
+                <td>123dgh23</td>
+                <td>10 kr</td>
+            </tr>
+            <tr>
+                <td>Hans Hansen</td>
+                <td>Suzuki</td>
+                <td>123dgh23</td>
+                <td>10 kr</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 
-<br> <br>
+<br>
+<br>
+<br>
 
-<table>
-    <tr>
-        <th>Customer Name</th>
-        <th>Phone no</th>
-        <th>Brand preference</th>
-        <th>Priority</th>
-        <th>From address</th>
-        <th>To address</th>
-    </tr>
 
-    <?php
+<!--Bottom bar with label and button -->
+<div class="flex-container" id = "bottomBar">
+    <div class="flex-item">
+        <label>
+            Share mode is OFF
+        </label>
+    </div>
 
-    echo "<tr>row
-    </tr>"
-    ?>
-    <tr>
-        <td>Jill Smith</td>
-        <td>12345678</td>
-        <td>Toyota</td>
-        <td>10</td>
-        <td>From this place</td>
-        <td>To this place</td>
-    </tr>
-    <tr>
-        <td>Jill Smith</td>
-        <td>12345678</td>
-        <td>Toyota</td>
-        <td>10</td>
-        <td>From this place</td>
-        <td>To this place</td>
-    </tr>
-    <tr>
-        <td>Jill Smith</td>
-        <td>12345678</td>
-        <td>Toyota</td>
-        <td>10</td>
-        <td>From this place</td>
-        <td>To this place</td>
-    </tr>
-</table>
+    <div class="flex-item">
+        <button>
+            Dispatch taxi
+        </button>
+    </div>
+
+
+
+</div>
+
+
 </body>
+
+
+<footer>
+    Developed by Rayan and Lise inc.
+</footer>
+
+
 </html>
 
 
