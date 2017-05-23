94f9c53018efa4ad0b8ced8edc47b90055e0792d
Lisesoee <Lise1005>
2017-05-23 10:10:55 +0200

    Pairprogramming building and testing RESTapi. We can now GET and DELETE successfully from postman.

--- RESTapi.php
+++ RESTapi.php
@@ -5,6 +5,7 @@
  * Date: 22-05-2017
  * Time: 14:29
  */
+include('Database.php');
 
 $Database = new Database();
 
@@ -12,9 +13,53 @@ $Database = new Database();
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
+//We get the table and key
+$table = array_shift($request);
+$key = array_shift($request);
+
+
 
 switch ($method){
     case 'GET':
-       // $sql = "SELECT * FROM '$table'"."($key? WHERE id =$key":''); break;
+        $sql = "SELECT * FROM `$table`";
+        //$sql = "SELECT * FROM `$table`".($key?" WHERE id =$key":``);
+        echo "Get some echo";
+        break;
+    case 'PUT':
+        break;
+    case 'POST':
+        break;
+    case 'DELETE':
+        $sql = "DELETE FROM `$table` WHERE Request_ID =$key";
+        break;
+}
+
+//Execute sql statement
+//$result = $Database -> doSelect($sql);
+$result = $Database -> doExecuteQuery($sql);
+
+echo "some more stuff";
+
+if (is_array($result)){
+    echo "ARRAY!";
+
+    $i=0;
+    foreach ($result as $item) {
+        echo $i;
+        $i++;
+        echo $result["FName"];
+        echo $result["LName"];
+    }
+}
+
+
+
+if ($method == 'GET') {
+    if (!$key) echo '[';
+    for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+        echo ($i > 0 ? ',' : '') . json_encode(mysqli_fetch_object($result));
+    }
+    if (!$key) echo ']';
+
 }
 
